7ccee49077d3b5c6fa19e1092e8e6c513e874324
Tusharjagi <tusharjagi@gmail.com>
2022-04-06 22:29:19 +0530

    String Length

---
+++ String/String_length.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int getLength(char name[])
+{
+    int count = 0;
+    for(int i = 0; name[i] != '\0' ; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    char name[20];
+    cout<<"Enter your name :: "<<endl;
+    cin>>name;
+    int count = 0 ;
+
+    cout<<"Your Name is ";
+    cout<<name<<endl; 
+
+    cout<<"Length: "<<getLength(name)<<endl;
+    
+    return 0;
+}
