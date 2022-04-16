b4d29830caacd3c47de38da854373700dc7db5f6
Tusharjagi <tusharjagi@gmail.com>
2022-04-16 15:30:25 +0530

    Empty

--- Recursion/Subsequences_String.cpp
+++ Recursion/Subsequences_String.cpp
@@ -1,23 +1,8 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void printSubsequence(string input, string output)
-{
-	if (input.empty()) {
-		cout << output << endl;
-		return;
-	}
-	printSubsequence(input.substr(1), output + input[0]);
-	printSubsequence(input.substr(1), output);
-}
-
-int main()
-{
+int main(){
 	
-	string output = "";
-	string input = "abcd";
-
-	printSubsequence(input, output);
-
 	return 0;
 }
